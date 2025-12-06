37137eb772a7bd8d37248072b1b33051a64de4fd
Th30n3and0nly <sw+github@nska.de>
2025-12-06 22:42:32 +0100

    Create substitutionsChecker.py

---
+++ substitutionsChecker.py
@@ -0,0 +1,31 @@
+import urllib.request
+import re
+from bs4 import BeautifulSoup as bs
+
+class SubstitutionsChecker:
+   
+    @staticmethod
+    def check(day, needles):
+  
+        urls = [
+            "https://vplan.jenaplan-weimar.de/Vertretungsplaene/SchuelerInnen/subst_001.htm",
+            "https://vplan.jenaplan-weimar.de/Vertretungsplaene/SchuelerInnen/subst_002.htm",
+            "https://vplan.jenaplan-weimar.de/Vertretungsplaene/SchuelerInnen/subst_003.htm"
+        ]
+        
+        page = urllib.request.urlopen(urls[day]).read()
+        soup = bs(page,'html.parser')
+        date = soup.body.find('div', attrs={'class' : 'mon_title'}).text
+    
+        tables = soup.find_all('table')
+        #print(tables)
+        headers = [header.text for header in tables[-1].find_all('th')]
+        substitutionPlan = list(filter(lambda x:x,[{headers[i]: cell.text for i, cell in enumerate(row.find_all('td'))} for row in tables[-1].find_all('tr')]))
+
+        substitutions = []      
+        for line in substitutionPlan:
+            for needle in needles:
+                if(re.search(needle, line["Gruppen"])):
+                    substitutions.append(line)
+
+        return [date, substitutions, urls[day]]
