be0b921e36eba83d833d724928e5d9ec291f971d
Th30n3and0nly <sw+github@nska.de>
2025-12-06 22:41:16 +0100

    fix From-Header violation

--- sendMail.py
+++ sendMail.py
@@ -2,21 +2,21 @@ import smtplib
 import os
 from email.mime.text import MIMEText
 
-class sendMail:
+class SendMail:
     port = os.getenv("EMAIL_SMTP_PORT")
-    smtp_server = os.getenv("EMAIL_SMTP_SERVER")
+    server = os.getenv("EMAIL_SMTP_SERVER")
     login = os.getenv("EMAIL_USER")
     password = os.getenv("EMAIL_PASSWORD")
-    sender_email = os.getenv("EMAIL_SENDER")
+    sender = os.getenv("EMAIL_SENDER")
 
     def __init__(self, receiver, subject, body): 
         self.message = MIMEText(body, "plain")
-        self.message["From"] = "Vertretungsplan Mailer Jenaplanschule Weimar"
+        self.message["From"] = self.sender
         self.message["Subject"] = subject
         self.message["To"] = receiver
         
     def send(self):
         print("Sending mail to: "+ self.message["To"])
-        with smtplib.SMTP_SSL(self.smtp_server, self.port) as server:
+        with smtplib.SMTP_SSL(self.server, self.port) as server:
             server.login(self.login, self.password)
-            server.sendmail(self.sender_email, self.message["To"], self.message.as_string())
+            server.sendmail(self.sender, self.message["To"], self.message.as_string())
