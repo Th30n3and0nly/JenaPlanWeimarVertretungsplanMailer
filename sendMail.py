c96ac8afa54f82d6c6f1dca6095a8cb6e9f6ff06
Th30n3and0nly <sw+github@nska.de>
2025-12-06 23:41:52 +0100

    Update sendMail.py

--- sendMail.py
+++ sendMail.py
@@ -3,9 +3,9 @@ import os
 from email.mime.text import MIMEText
 
 class SendMail:
-    port = os.getenv("EMAIL_SMTP_PORT")
     server = os.getenv("EMAIL_SMTP_SERVER")
-    login = os.getenv("EMAIL_USER")
+    port = os.getenv("EMAIL_SMTP_PORT")
+    user = os.getenv("EMAIL_USER")
     password = os.getenv("EMAIL_PASSWORD")
     sender = os.getenv("EMAIL_SENDER")
 
@@ -18,5 +18,5 @@ class SendMail:
     def send(self):
         print("Sending mail to: "+ self.message["To"])
         with smtplib.SMTP_SSL(self.server, self.port) as server:
-            server.login(self.login, self.password)
+            server.login(self.user, self.password)
             server.sendmail(self.sender, self.message["To"], self.message.as_string())
